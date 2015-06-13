ce3bf7adb5ef9eb877e7f307004070ba31ba08b2
Existanza <kraft.sc2@gmail.com>
2015-06-13 18:04:38 +0200

    ~

--- ppeval.py
+++ ppeval.py
@@ -6,120 +6,88 @@ import sqlite3
 
 key_file = open('C:/pyton/keys/osu.txt', 'r')
 api_key = key_file.read()
-# save = open('C:/pyton/ppeval/osu.txt', 'a')
 user = '2063607'
-limit = '1'  # 1-50
-mode = '1'  # 0 = osu!, 1 = Taiko, 2 = CtB, 3 = osu!mania
+limit = '3'  # 1-50
+game_mode = '1'  # 0 = osu!, 1 = Taiko, 2 = CtB, 3 = osu!mania
 convert = '0'   # specify whether converted beatmaps are included (0 = not included, 1 = included).
 # Only has an effect if m is chosen and not 0. Optional, default is 0.
-conn = sqlite3.connect('taiko.db')
+conn = sqlite3.connect('taikopersonal2015.db')
 c = conn.cursor()
 
-
-# def beatmap_info(map_id, user_score):
-def beatmap_info(map_id):
-    map_url = 'https://osu.ppy.sh/api/get_beatmaps' + '?k=' + api_key + '&b=' + map_id + '&m=' + mode + '&a=' + convert
+def beatmap_info(map_id, user_score):
+    map_url = 'https://osu.ppy.sh/api/get_beatmaps' + '?k=' + api_key + '&b=' + map_id + '&m=' + game_mode + '&a=' + convert
+    print('Getting map info: ' + map_id)
     rc_map = requests.get(map_url)
     map_info = ast.literal_eval(rc_map.text)
     if map_info:
         for i in range(len(map_info)):
             dict = ast.literal_eval(str(map_info[i]))
-            for key, val in dict.items():
-                if key == 'title':
-                    title = val
-                if key == 'version':
-                    version = val
-                if key == 'approved':
-                    ranked = val
-                if key == 'last_update':
-                    last_update = val
-                if key == 'difficultyrating':
-                    stars = val
-                if key == 'bpm':
-                    bpm = val
-                if key == 'hit_length':
-                    hit_length = val
-        # print(map_id + ' - ' + title + ' - ' + version)
-        # save.write(map_id + ' - ' + title + ' - ' + version + '\n')
-        # print(user_score, end=' -> ')
-        # save.write(user_score + ' -> ')
-        if ranked == '1':
-            get_scores(map_id, title, version, last_update, stars, bpm, hit_length)
+            if dict['approved'] == '1':
+                ret = get_scores(map_id, dict['title'], dict['version'], dict['approved_date'], dict['difficultyrating'],
+                           dict['bpm'], dict['hit_length'], user_score)
+                return ret
 
 
-def get_scores(key, title, version, last_update, stars, bpm, hit_length):
+def get_scores(key, title, version, approved_date, stars, bpm, hit_length, user_score):
     scores_url = 'https://osu.ppy.sh/api/get_scores' + '?k=' + api_key + '&b=' + key + '&m=1'
+    print('Getting scores: ' + key)
     rc2 = requests.get(scores_url)
     res2 = ast.literal_eval(rc2.text)
-    score = 0
-    hr, hr_found, dt, dt_found, hrdt, hrdt_found, no_mod, no_mod_found = (False,)*8
+    hr_found, dt_found, hrdt_found, no_mod_found = (False,)*4
     for i in range(len(res2)):
         dic = ast.literal_eval(str(res2[i]))
-        for key2, val2 in dic.items():
-            if key2 == 'pp':
-                score = val2
-            if key2 == 'enabled_mods' and val2 == '16':
-                hr = True
-            if key2 == 'enabled_mods' and val2 == '64':
-                dt = True
-            if key2 == 'enabled_mods' and val2 == '80':
-                hrdt = True
-            if key2 == 'enabled_mods' and val2 == '0':
-                no_mod = True
-        if hr and not hr_found:
-            # print(score + ' (HR) ', end='')
-            # save.write(score + ' (HR) ')
-            mod = 'HR'
-            c.execute('''INSERT INTO maps VALUES (?,?,?,?,?,?,?,?,?)''',
-                      (key, title, version, last_update, stars, score, mod, bpm, hit_length))
-            hr_found = True
-        if dt and not dt_found:
-            # print(score + ' (DT) ', end='')
-            # save.write(score + ' (DT) ')
-            mod = 'DT'
-            c.execute('''INSERT INTO maps VALUES (?,?,?,?,?,?,?,?,?)''',
-                      (key, title, version, last_update, stars, score, mod, bpm, hit_length))
-            dt_found = True
-        if hrdt and not hrdt_found:
-            # print(score + ' (HRDT) ', end='')
-            # save.write(score + ' (HRDT) ')
-            mod = 'HRDT'
-            c.execute('''INSERT INTO maps VALUES (?,?,?,?,?,?,?,?,?)''',
-                      (key, title, version, last_update, stars, score, mod, bpm, hit_length))
-            hrdt_found = True
-        if no_mod and not no_mod_found:
-            # print(score, end='')
-            # save.write(score)
-            mod = ''
-            c.execute('''INSERT INTO maps VALUES (?,?,?,?,?,?,?,?,?)''',
-                      (key, title, version, last_update, stars, score, mod, bpm, hit_length))
-            no_mod_found = True
-    print(last_update)
-    # print('\n')
-    # save.write('\n\n')
+        score = dic['pp']
+        mode = dic['enabled_mods']
+        mode_dic = {'0': '', '16': 'HR', '64': 'DT', '80': 'HRDT', '576': 'DT', '592': 'HRDT'}
+        if mode in mode_dic:
+            if mode_dic[mode] == 'HR' and not hr_found:
+                c.execute('''INSERT INTO maps VALUES (?,?,?,?,?,?,?,?,?,?)''',
+                          (key, title, version, approved_date, stars, score, mode_dic[mode], bpm, hit_length, user_score))
+                hr_found = True
+            elif mode_dic[mode] == 'DT' and not dt_found:
+                c.execute('''INSERT INTO maps VALUES (?,?,?,?,?,?,?,?,?,?)''',
+                          (key, title, version, approved_date, stars, score, mode_dic[mode], bpm, hit_length, user_score))
+                dt_found = True
+            elif mode_dic[mode] == 'HRDT' and not hrdt_found:
+                c.execute('''INSERT INTO maps VALUES (?,?,?,?,?,?,?,?,?,?)''',
+                          (key, title, version, approved_date, stars, score, mode_dic[mode], bpm, hit_length, user_score))
+                hrdt_found = True
+            elif mode_dic[mode] == '' and not no_mod_found:
+                c.execute('''INSERT INTO maps VALUES (?,?,?,?,?,?,?,?,?,?)''',
+                          (key, title, version, approved_date, stars, score, mode_dic[mode], bpm, hit_length, user_score))
+                no_mod_found = True
+    return approved_date
 
-# best_url = 'https://osu.ppy.sh/api/get_user_best' + '?k=' + api_key + '&u=' + user + '&m=' + mode + '&limit=' + limit
-# rc = requests.get(best_url)
-# res = ast.literal_eval(rc.text)
-# for i in range(len(res)):
-#     dic = ast.literal_eval(str(res[i]))
-#     for key, val in dic.items():
-#         if key == 'beatmap_id':
-#             beatmap_id = val
-#         if key == 'pp':
-#             pp = val
-#     beatmap_info(beatmap_id, pp)
 
-maps_url = 'https://osu.ppy.sh/api/get_beatmaps' + '?k=' + api_key + '&m=' + mode + '&a=' + convert + '&since=2015-04-08&limit=200'
-rc_maps = requests.get(maps_url)
-maps_info = ast.literal_eval(rc_maps.text)
-for i in range(len(maps_info)):
-    print(str(i) + '/' + str(len(maps_info)))
-    dic = ast.literal_eval(str(maps_info[i]))
-    for key, val in dic.items():
-        if key == 'beatmap_id':
-            print(val)
-            beatmap_info(val)
+def user_best(user, game_mode, limit):
+    best_url = 'https://osu.ppy.sh/api/get_user_best' + '?k=' + api_key + '&u=' + user + '&m=' + game_mode + '&limit=' + limit
+    rc = requests.get(best_url)
+    res = ast.literal_eval(rc.text)
+    for i in range(len(res)):
+        di = ast.literal_eval(str(res[i]))
+        beatmap_info(di['beatmap_id'], di['pp'])
+
+
+def get_all(game_mode):
+    date = '2005-01-01'
+    total = 0
+    while date is not None:
+        maps_url = 'https://osu.ppy.sh/api/get_beatmaps' + '?k=' + api_key + '&m=' + game_mode + \
+                   '&a=' + convert + '&since=' + date + '&limit=200'
+        rc_maps = requests.get(maps_url)
+        maps_info = ast.literal_eval(rc_maps.text)
+        for i in range(len(maps_info)):
+            print(str(total + i) + '/' + str(total + len(maps_info)))
+            di = ast.literal_eval(str(maps_info[i]))
+            date = beatmap_info(di['beatmap_id'], '0')
+            if date is None:
+                break
+            print(date)
+        total += len(maps_info)
+
+
+# user_best(user, game_mode, limit)
+get_all(game_mode)
 
 conn.commit()
 i = 0
@@ -127,4 +95,3 @@ for row in c.execute('''SELECT * FROM maps ORDER BY score'''):
     i += 1
 print(i)
 conn.close()
-# save.close()
--- read.py
+++ read.py
@@ -2,22 +2,30 @@ __author__ = 'Existanza'
 
 import sqlite3
 
-f = open("C:/pyton/ppeval/osu.txt", "w")
-conn = sqlite3.connect('taiko.db')
-c = conn.cursor()
-print('jaka piekna baza danych:')
-i = 0
-for row in c.execute('''SELECT * FROM maps ORDER BY score'''):
-    print(row)
-    f.write(str(row)+'\n')
-    i += 1
-print(i)
-conn.close()
-f.close()
 
-# conn = sqlite3.connect('taiko.db')
-# c = conn.cursor()
-# c.execute('''CREATE TABLE maps (map_id integer, map_name text, diff_name text, last_update text,
-#                                 stars real, score real, mode text, bpm integer, hit_length integer)''')
-# conn.commit()
-# conn.close()
+def old_db():
+    f = open("C:/pyton/ppeval/osu.txt", "w")
+    conn = sqlite3.connect('taikopersonal2015.db')
+    c = conn.cursor()
+    print('jaka piekna baza danych:')
+    i = 0
+    for row in c.execute('''SELECT * FROM maps ORDER BY score'''):
+        print(row)
+        f.write(str(row)+'\n')
+        i += 1
+    print(i)
+    conn.close()
+    f.close()
+
+
+def new_db():
+    conn = sqlite3.connect('taikopersonal2015.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE maps (map_id integer, map_name text, diff_name text, last_update text, stars real,
+                                    score real, mode text, bpm integer, hit_length integer, user_score real)''')
+    conn.commit()
+    conn.close()
+
+
+old_db()
+# new_db()
